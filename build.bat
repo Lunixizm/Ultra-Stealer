af089a02a75d8a488c7f18b5b509f714edcdd32b
Lunix <cyln.elif1@gmail.com>
2024-09-21 23:14:53 +0300

    building fix

--- build.bat
+++ build.bat
@@ -1,2 +1,8 @@
 pip install pyinstaller
 pyinstaller --onefile --noconsole --clean --manifest=uac_admin.manifest .\main.py
+set scriptDir=%~dp0
+powershell -Command "Remove-Item -Path '%scriptDir%build' -Recurse -Force"
+DEL main.spec
+cd /d dist
+move main.exe ..
+cd ..
